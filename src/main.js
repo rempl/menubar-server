ea980ed118f65a4e4a10120028303e184bba42d2
tyanas <tyanas.go@gmail.com>
2016-12-09 02:13:47 +0300

    change port

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ var app = electron.app;
 var Menu = electron.Menu;
 var MenuItem = electron.MenuItem;
 var Tray = electron.Tray;
+var BrowserWindow = electron.BrowserWindow;
 
 var fork = require('child_process').fork;
 var shell = require('electron').shell;
-var PORT = 8003;
+var PORT = 8177;
+var port = PORT;
 
 var platform = process.platform === 'win32' ? 'windows' : 'mac';
 var iconPath = {
@@ -15,23 +17,39 @@ var iconPath = {
     inactive: path.resolve(__dirname, `../res/${platform}-icon-gray.png`)
 };
 
-var appIcon;
+var appIcon = null;
 var hasTab = false;
-var server;
+var server = null;
+var win = null;
+var contextMenu = null;
 
 function startServer() {
     server = fork(path.resolve(__dirname, './launch.js'));
-    server.send(`launch ${PORT}`);
+    server.send(`launch ${port}`);
+
     if (appIcon) {
         appIcon.setImage(iconPath.active);
+    }
+
+    if (contextMenu) {
+        contextMenu.items[0].enabled = false;
+        contextMenu.items[1].enabled = true;
+        contextMenu.items[2].enabled = true;
     }
 }
 
 function killServer() {
     if (server) {
         server.kill();
+
         if (appIcon) {
             appIcon.setImage(iconPath.inactive);
+        }
+
+        if (contextMenu) {
+            contextMenu.items[0].enabled = true;
+            contextMenu.items[1].enabled = false;
+            contextMenu.items[2].enabled = false;
         }
     } else {
         console.log('Nothing to stop');
@@ -41,9 +59,21 @@ function killServer() {
 function showTab() {
     setTimeout(function() {
         hasTab = true;
-        shell.openExternal(`http://localhost:${PORT}/basisjs-tools/devtool/`);
+        shell.openExternal(`http://localhost:${port}/basisjs-tools/devtool/`);
     }, 500);
 }
+
+function sync(newPort) {
+    port = newPort;
+    killServer();
+    startServer();
+    showTab();
+}
+
+global.config = {
+    port: port,
+    sync: sync
+};
 
 // available on MacOS only
 if (app.dock) {
@@ -51,18 +81,20 @@ if (app.dock) {
 }
 
 app.on('ready', function() {
+    win = new BrowserWindow({ show: false , width: 300, height: 80, frame: false });
+    win.setPosition(1000, 25);
+    win.loadURL('file://' + __dirname + '/index.html');
+
     appIcon = new Tray(iconPath.inactive);
 
     startServer();
 
     var startMenuItem = new MenuItem({
-        label: `Start at ${PORT}`,
+        label: `Start rempl`,
         enabled: false,
         click: function() {
             if (!server || server.killed || !server.connected) {
                 startServer();
-                contextMenu.items[0].enabled = false;
-                contextMenu.items[1].enabled = true;
             }
 
             if (!hasTab) {
@@ -71,23 +103,27 @@ app.on('ready', function() {
         }
     });
     var stopMenuItem = new MenuItem({
-        label: 'Stop',
+        label: `Stop rempl`,
         enabled: true,
-        sublabel: `Running at ${PORT}`,
         click: function() {
             console.log('click stop');
             killServer();
-            contextMenu.items[1].enabled = false;
-            contextMenu.items[0].enabled = true;
         }
     });
     var contextMenu = Menu.buildFromTemplate([
         startMenuItem,
         stopMenuItem,
         {
-            label: 'Open new tab',
+            label: 'Open sandbox',
+            enabled: true,
             click: function() {
                 showTab();
+            }
+        },
+        {
+            label: 'Change port',
+            click: function() {
+                win.show();
             }
         },
         {
